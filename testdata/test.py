9b44bc5bd203d2f7b81849a9136aff9ec0494be3
Stefan Joachimsthaler <stefanjoachimsthaler@gmail.com>
2024-03-16 16:56:00 +0100

    add testdata

---
+++ testdata/test.py
@@ -0,0 +1,17 @@
+with open('measurements.xml', 'r') as file:
+    meter_r = file.read()
+
+
+import xmltodict
+test = str(meter_r)
+print(meter_r)
+
+print(test)
+
+meter_data = xmltodict.parse(test)
+print(meter_data)
+
+import json
+
+with open('test.json', 'w') as file:
+    json.dump(meter_data, file)
